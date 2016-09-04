3744e90d46bbc839c151bc35d4a7679447252eef
Isidorito <isidorito@outlook.com>
2016-09-04 03:59:15 -0300

    9 Se pusieron en funcionamiento los botones de modificacion, delete, y activar

--- app/controllers/accommodation_types_controller.rb
+++ app/controllers/accommodation_types_controller.rb
@@ -1,23 +1,31 @@
 class AccommodationTypesController < ApplicationController
 
 	def create 
+
 		AccommodationType.create(params.require(:accommodation_type).permit(:nombre,:activo))
 
 	end
 
 	def edit
-		AccommodationType.edit(params.require(:accommodation_type).permit(:nombre,:activo))
+		@h = AccommodationType.find(params.require(:id))
+		
 		
 	end
 
 	def destroy
-		AccommodationType.delete(params.require(:accommodation_type).permit(:nombre,:activo))
+		@h = AccommodationType.find(params.require(:id))
+		AccommodationType.destroy(@h.id)
+		#AccommodationType.where(:id => (params.require(:id))).delete(params.require(:accommodation_type).permit(:nombre,:activo))
+		#AccommodationType.delete(params.require(:accommodation_type).permit(:nombre,:activo))
+		redirect_to :back
 	end
 
 
 	def update
-		
-		@tipoHospedaje = AccommodationType.new		
+		#@h = AccommodationType.find(:id)
+		AccommodationType.where(:id => (params.require(:id))).update(params.require(:accommodation_type).permit(:nombre,:activo))
+
+		#AccommodationType.update(params.require(:accommodation_type).permit(:id,:nombre,:activo))		
 
 	end
 
@@ -31,7 +39,26 @@ class AccommodationTypesController < ApplicationController
 	end
 
 	def admin
-		@tipoHospedaje = AccommodationType.all
+		@tipoHospedaje = AccommodationType.all.order("accommodation_types.id ASC")
+		
+	end
+
+	def show
+		@h = AccommodationType.find(params.require(:id))
+		if @h.activo == true
+
+			#:activo => false
+			#AccommodationType.where(:id => (params.require(:id))).update(params.require(:accommodation_type).permit(:nombre,:activo => 'false'))
+			@h.update(:activo => 'false')
+		else
+			
+			#:activo => true
+			#AccommodationType.where(:id => (params.require(:id))).update(params.require(:accommodation_type).permit(:nombre,:activo => 'true'))
+			@h.update(:activo => 'true')
+		end
+
+		#AccommodationType.where(:id => (params.require(:id))).update(params.require(:accommodation_type).permit(:nombre,:activo))
+		redirect_to :back
 		
 	end
 end
