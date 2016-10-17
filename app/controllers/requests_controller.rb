6d21723c6feb6aaffd7bf8b58ab1955ce72d1cef
Isidorito <isidorito@outlook.com>
2016-10-17 08:32:42 -0300

    20 Creacion de Solicitudes  - NO carga las fechas

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,2 +1,14 @@
 class RequestsController < ApplicationController
+
+	def create
+	
+		Request.create(params.permit(:accommodation_id, :accommodation_owner, :requester, :initial_date, :final_date))
+	
+	end
+
+	def show
+
+		@Solicitud = Request.where(:accommodation_owner => current_user.id).order("id ASC") 
+		
+	end
 end
