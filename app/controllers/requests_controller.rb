c44e4b147dc387408e833e95bcea56a8de0c2a9b
Isidorito <isidorito@outlook.com>
2016-11-01 03:00:58 -0300

    24 Eliminacion solicitudes redundantes - cambio de imagenes - impresion de imagen en index

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -3,14 +3,22 @@ class RequestsController < ApplicationController
 	def create
 	
 		Request.create(params.require(:request).permit(:accommodation_id, :accommodation_owner, :requester, :initial_date, :final_date))
+
+		redirect_to :back
 	
 	end
 
 	def show
 
-		@Solicitud = Request.where(:accommodation_owner => current_user.id).where(:ok => false).order("id ASC") 
+		@todas = Request.where(:accommodation_id => params[:id])
 
-		@Aceptadas = Request.where(:accommodation_owner => current_user.id).where(:ok => true).order("id ASC")
+
+		@Solicitud = @todas.where(:ok => false).order("id ASC")
+		#(:accommodation_owner => current_user.id).where(:ok => false).order("id ASC") 
+		
+
+		@Aceptadas = @todas.where(:ok => true).order("id ASC")
+		#(:accommodation_owner => current_user.id).where(:ok => true).order("id ASC")
 		
 	end
 
@@ -26,23 +34,25 @@ class RequestsController < ApplicationController
 
 	def edit
 
-		@f = Request.where(:id => params[:id])
+		@f = Request.where(:id => params[:id])	
+		
 
 		@f.update(:ok => 'true')
 		########################
 		#Elimina Otras solicitudes existentes que caigan en el mismo periodo que el de la aceptada
 
-			r1 = Request.where('initial_date >= ? AND initial_date <= ?', params[:initial_date], params[:final_date])
-			r2 = Request.where('final_date >= ? AND final_date <= ?', params[:initial_date], params[:final_date])
+		@f.each do |f|
 
-			r1.destroy.all
-			r2.destroy.all
+			r1 = Request.where('initial_date >= ? AND initial_date <= ? AND ok = false', f.initial_date, f.final_date)
 
+			r2 = Request.where('final_date >= ? AND final_date <= ? AND ok = false', f.initial_date, f.final_date)
+			 
+			r1.destroy_all
+			r2.destroy_all
 
-
-
+		end
 		########################
-		render :show
+		redirect_to :back
 
 	end	
 end
